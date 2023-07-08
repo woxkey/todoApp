bcb7322ba8ae0041f6979cb5d7a51c7205cffead
Alen <bolatovalen@gmail.com>
2023-07-08 12:35:33 +0600

    sdfasdfds

---
+++ src/views/TodosView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todos">
+    <h1>This is an todo page</h1>
+    <Todos />
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+import Todos from '../components/Todos.vue'
+
+@Component({
+  components: {
+    Todos
+  }
+})
+export default class TodosView extends Vue {}
+</script>
